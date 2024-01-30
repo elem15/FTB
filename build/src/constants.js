85cfe9a5bb4e6539b122ac36360380a248a3c688
Elem <elem15@yandex.ru>
2024-01-30 18:42:14 +0300

    refactor: delete build from git

--- build/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET_KEY = exports.PORT = void 0;
-exports.PORT = 3500;
-exports.SECRET_KEY = 'myVerySecretKey-04.05.2022';
